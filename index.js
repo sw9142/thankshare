5c6527721638653c58590a975d3e04478a355d1d
sewon <sw9142@gmail.com>
2021-07-02 12:58:29 +0900

    made change on index.js

--- index.js
+++ index.js
@@ -42,14 +42,18 @@ app.use(cors(corsOptions));
 if (process.env.NODE_ENV === "production") {
   // Set static folder
   // All the javascript and css files will be read and served from this folder
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "client/build")));
 
   // index.html for all page routes  html or routing and naviagtion
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-  });
+  app.get('*', function (req, res) {
+    const fullPath = path.join(__dirname,  '../client', 'build', 'index.html')
+    res.sendFile(fullPath)
+  })
 }
+
+
+
+
 
 
 
