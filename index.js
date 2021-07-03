d061bdea36356511fd5e74f153a28ab948d95e50
sewon <sw9142@gmail.com>
2021-07-03 20:25:14 +0900

    16th modification

--- index.js
+++ index.js
@@ -34,7 +34,6 @@ app.use(
       defaultSrc: ["'self'"],
       scriptSrc: ["'self'", "'unsafe-inline'"],
       styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.gstatic.com/*", "https://fonts.googleapis.com/css?family=Pacifico&display=swap","data:"],
-      fontSrc: ["data:"],
     },
   })
 );
