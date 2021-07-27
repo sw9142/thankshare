0b12690858b473bd78a53fb57b1666058a7d3deb
sewon <sw9142@gmail.com>
2021-07-27 11:44:44 +0900

    fix bugs

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
 
-
-mongoose.connect(config.mongoURI, {
+mongoose
+  .connect(config.mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
@@ -18,7 +18,6 @@ mongoose.connect(config.mongoURI, {
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
-
 dotenv.config();
 
 app.use(cookieParser());
@@ -26,37 +25,11 @@ app.use(cookieParser());
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
-
-
-
 const corsOptions = {
   origin: "https://ibibles.net/",
   credential: true,
 };
 app.use(cors(corsOptions));
-
-
-
-
-// Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  // All the javascript and css files will be read and served from this folder
-  app.use(express.static(path.join(__dirname, "/client/build")));
-
-  // index.html for all page routes  html or routing and naviagtion
-  app.get('*', function (req, res) {
-    const fullPath = path.join(__dirname,  '../client', 'build', 'index.html')
-    res.sendFile(fullPath)
-  })
-}
-
-
-
-
-
-
-
 
 // parse application/json
 
@@ -65,14 +38,24 @@ app.use("/api/users", require("./server/controller/users"));
 app.use("/api/msg", require("./server/controller/message"));
 app.use("/api/profile", require("./server/controller/profile"));
 
-app.get("/", (req, res) => {
-  res.send("hello??!");
-});
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  // All the javascript and css files will be read and served from this folder
+  app.use(express.static(path.join(__dirname, "/client/build")));
+
+  // index.html for all page routes  html or routing and naviagtion
+  app.get("*", function (req, res) {
+    console.log("hi?");
+    const fullPath = path.join(__dirname, "/client", "build", "index.html");
+    console.log("get  '*'????");
+    res.sendFile(fullPath);
+  });
+}
 
 PORT = process.env.PORT || 5000;
 
 app.listen(PORT, (err) => {
-
   if (err) console.log("err", err);
   console.log("Successfully Connected To Port ", PORT);
 });
